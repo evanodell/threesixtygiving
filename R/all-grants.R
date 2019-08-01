dd46fb0b244d227db114192a85dcf5e3087adab3
evan <evanodell91@gmail.com>
2019-08-01 14:12:45 +0100

    adding pipe

--- R/all-grants.R
+++ R/all-grants.R
@@ -6,7 +6,7 @@
 #' Returns a list of data frames with details of all grants from funders
 #' returned by [tsg_available()].
 #'
-#' @param with_meta If `TRUE`
+#' @param core_data If `TRUE`
 #' @param verbose If `TRUE`, prints console messages.
 #'
 #' @return A list of data frames.
@@ -15,13 +15,79 @@
 #' @examples \donttest{
 #' all_grants <- tsg_all_grants()
 #' }
-tsg_all_grants <- function(with_meta = TRUE, verbose = TRUE) {
+
+
+tsg_all_grants <- function(core_data = FALSE, verbose = TRUE) {
   grant_df <- tsg_available()
 
-  df <- tsg_data_retrieval(grant_df, verbose = verbose)
+  df <- threesixtygiving:::tsg_data_retrieval(grant_df, verbose = verbose)
 
-   if (with_meta) {
-  #   df2 <- tibble::tibble(grant_df, df)
+   if (core_data) {
+      # get all the common spending information into a single dataset
    }
   df
 }
+# attempts to sort out this whole nested data frame problem
+# library(dplyr)
+# x <- as_tibble(unlist(lapply(df, names)))
+#
+#
+# x2 <- x %>% group_by(value) %>% summarise(n=n()) %>% arrange(desc(n))
+#
+# x2
+#
+# df2 <- df
+# for (i in 1:216) {
+#
+# df2[[i]][, ] <- lapply(df2[[i]][, ], as.character)
+#
+# }
+#
+# df2 <- bind_rows(df2) ## check what is missing common names
+#
+# df_bind <- bind_rows(df)
+#
+# df_missing <- df2 %>% filter(is.na(recipient_org_identifier))
+#
+# summary(factor(df_missing$id2))
+#
+# df_missing2 <- df_missing[,colSums(is.na(df_missing))<nrow(df_missing)]
+#
+# unique(df_missing2$id2)
+#
+# t2 <- df[[38]]
+#
+# t1 %>% unnest(recipient_organization)
+#
+# x <- bind_rows(t1$recipient_organization)
+#
+# t2xx <- t2  %>%
+#   mutate_if(is.list, bind_rows)
+#
+#
+# ss <- t2 %>% mutate(Value = map(recipient_organization, rownames_to_column, "a"))
+#
+# ss$Value
+#
+# df1 <- tibble(
+#   gr = c('a', 'b', 'c'),
+#   values = list(1:2, 3:4, 5:6)
+# )
+#
+#
+# t2 %>% map_df(recipient_organization, as_tibble)
+#
+# df1 %>%
+#   mutate(r = map(., ~ data.frame(t(.)))) %>%
+#   unnest(r) %>%
+#   select(-values)
+#
+#
+# library(tidyverse)
+#
+# df6 %>%
+#   mutate(Value = map(Value, as.data.frame),
+#          Value = map(Value, rownames_to_column, 'a'),
+#          Value = map(Value, ~gather(., b, value, -a))) %>%
+#   unnest(Value) %>%
+#   complete(Step, a, b)
--- R/utils-retrieve-datasets.R
+++ R/utils-retrieve-datasets.R
@@ -3,10 +3,12 @@
 # tsg_specific_data
 tsg_data_retrieval <- function(query_df, verbose = verbose) {
 
+  query_df$id2 <- c(1:nrow(query_df))
+
   spend_df <- list()
 
   for (i in seq_along(query_df$title)) {
-    if (verbose) {
+    if (!verbose) {
       message(paste0("Downloading ", i, " of ", length(query_df$title)))
     }
 
@@ -27,7 +29,7 @@ tsg_data_retrieval <- function(query_df, verbose = verbose) {
           )
       } else {
 
-        spend_df[[query_df$identifier[[i]]]] <- tryCatch({ ## majority of returns
+        spend_df[[i]] <- tryCatch({ ## majority of returns
           readxl::read_excel(temp_f, col_types = "text")
         },
         error = function(cond) {
@@ -56,17 +58,15 @@ tsg_data_retrieval <- function(query_df, verbose = verbose) {
         )
       } else if (suffix == "json") { ## only a handful of JSON files
         spend_df[[i]] <- tryCatch({
-          jsonlite::fromJSON(query_df$download_url[[i]])
+          jsonlite::fromJSON(query_df$download_url[[i]], flatten = TRUE)
         },
         error = function(cond) {
           return("Download failed")
         }
         )
 
-        if (is.list(spend_df[[i]])) {
-          spend_df[[i]] <- spend_df[[i]]$grants
-
-          spend_df[[i]][, ] <- lapply(spend_df[[i]][, ], as.character)
+        if (is.list(spend_df[[i]]) && is.data.frame(spend_df[[i]][[1]])) {
+          spend_df[[i]] <- dplyr::as_tibble(spend_df[[i]][[1]])
         }
       }
 
@@ -74,9 +74,61 @@ tsg_data_retrieval <- function(query_df, verbose = verbose) {
 
     names(spend_df[[i]]) <- snakecase::to_snake_case(names(spend_df[[i]]))
 
+    spend_df[[i]]$id2 <- i
+
+    # insert an option to do this or not?
+
+     if (any(lapply(spend_df[[i]], class)=="list")) {
+       x_list <- dplyr::select_if(s, is.list) # select list cols
+
+
+        x_list <- dplyr::select_if(spend_df[[i]], is.list) # select list cols
+
+        de <- dplyr::as_tibble(lapply(x_list, sapply, nrow))
+
+        de <- de %>%
+          dplyr::mutate_if(is.list, as.character) %>%
+          dplyr:: mutate_if(is.character, as.numeric) %>%
+          dplyr::select(which(colMeans(., na.rm = TRUE) <= 1))
+
+        de2 <- dplyr::as_tibble(lapply(x_list, sapply, length))
+
+        de2 <- de2 %>%
+          dplyr::mutate_if(is.list, as.character) %>%
+          dplyr:: mutate_if(is.character, as.numeric)  %>%
+          dplyr::select(one_of(names(de))) %>%
+          dplyr::select(which(colMeans(., na.rm = TRUE) <= 1))
+
+        #x_list[x_list == "NULL"] <- tibble::tibble(a = character())
+
+        x_list[x_list == "NULL"] <- NA
+
+        lapply(x_list, is.na)
+
+        rrr <-rapply( x_list, f=function(x) ifelse(is.na(x),0,x), how="replace" )
+
+        x_list2 <- x_list %>%
+           dplyr::select(names(de))  %>%
+           dplyr::mutate_at(vars(names(de2)), unlist)  %>%
+           dplyr::mutate_at(vars(names(de2)), as.list)
+
+         y_list <- purrr::map_if(x_list2, is.list, bind_rows) %>%
+           dplyr::as_tibble() %>%
+           jsonlite::flatten() %>%
+           dplyr::as_tibble() %>%
+           janitor::remove_empty(which = "cols")
+
+         x_list3 <- dplyr::select(x_list, -one_of(names(x_list2)))
+
+         orig_df <- dplyr::select(spend_df[[i]], -one_of(names(x_list)))
+
+         spend_df[[i]] <- bind_rows(orig_df, y_list, x_list3)
+
+     }
+
   }
 
-  names(spend_df) <- query_df$identifier
+  #names(spend_df) <- query_df$identifier
 
   spend_df
 
